c5cddcb9734e7ec3293fb48539288daa9779c4ae
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-02-10 01:04:06 +0530

    Init

---
+++ app/design/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import CardsDefault from "~/app/_components/design"
+
+export default function Home() {
+  return (
+    <div className="max-w-screen-2xl mx-auto my-20">
+
+  <CardsDefault/>
+    </div>
+  )
+}
---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env.js";
+
+export default {
+  schema: "./server/db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: env.DATABASE_URL,
+  },
+  tablesFilter: ["astra_*"],
+} satisfies Config;
