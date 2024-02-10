cf08a682a592bfc107a6b37e9d215d0d8bb0990b
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-02-10 21:08:53 +0530

    init

--- app/_components/design/metric.tsx
+++ app/_components/design/metric.tsx
@@ -63,7 +63,7 @@ export function CardsMetric() {
             >
               <Tooltip
                 content={({ active, payload }) => {
-                  if (active && payload && payload.length) {
+                  if (active && payload?.length) {
                     return (
                       <div className="rounded-lg border bg-background p-2 shadow-sm">
                         <div className="grid grid-cols-2 gap-2">
@@ -72,7 +72,7 @@ export function CardsMetric() {
                               Average
                             </span>
                             <span className="font-bold text-muted-foreground">
-                              {payload[0].value}
+                              {payload[0]?.value}
                             </span>
                           </div>
                           <div className="flex flex-col">
@@ -80,7 +80,7 @@ export function CardsMetric() {
                               Today
                             </span>
                             <span className="font-bold">
-                              {payload[1].value}
+                              {payload[1]?.value}
                             </span>
                           </div>
                         </div>
