c4287896b604367ea17392249e12ab81790f61ef
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2024-02-13 12:35:00 +0530

    feat: intro home

---
+++ app/home/_components/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { BeautyOfSJCET } from "./beauty-of-sjcet";
+
+interface HomeProps {
+    
+}
+ 
+const Home: FC<HomeProps> = () => {
+    return ( 
+        <BeautyOfSJCET/>
+     );
+}
+ 
+export default Home;
---
+++ app/home/page.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { BeautyOfSJCET } from "./_components/beauty-of-sjcet";
+
+interface HomeProps {
+    
+}
+ 
+const Home: FC<HomeProps> = () => {
+    return ( 
+        <BeautyOfSJCET/>
+     );
+}
+ 
+export default Home;
